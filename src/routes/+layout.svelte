a73c21f008eea8fb61fe0d2a9c4df5955572ddd3
Morghan J <Arsoth@users.noreply.github.com>
2023-07-28 18:16:56 -0400

    Zed fix, removed extra div

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,51 +47,49 @@
 		>
 		<Menu class="md:hidden" menuOpen={sidebarOpen} on:click={() => (sidebarOpen = !sidebarOpen)} />
 	</div>
-	<div class="flex flex-col text-gray-700 z-10 bg-white flex-shrink-0 fixed top-14 shadow-xl">
+	<div class="flex flex-col text-gray-700 z-[9] bg-white flex-shrink-0 fixed top-14 shadow-xl">
 		{#if sidebarOpen || innerWidth >= breakpoints.md}
 			<nav
 				transition:slide={{ delay: 10, duration: 150 }}
 				class:shadow-lg={!sidebarOpen}
 				class="flex-grow px-4 pb-4 w-screen md:w-64 md:min-h-screen md:pb-0 md:overflow-y-auto z-100 bg-gray-100"
 			>
-				<div class="relative">
-					<button
-						on:click|stopPropagation={() => (dropdownOpen = !dropdownOpen)}
-						class="py-0.5 mt-1 parent w-full"
-					>
-						<span class:!bg-gray-200={isCalcPage} class="menuItem child transition-all">
-							<span>Calculators</span>
-							<svg
-								fill="currentColor"
-								viewBox="0 0 20 20"
-								class:rotate-180={dropdownOpen}
-								class:rotate-0={!dropdownOpen}
-								class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-150 transform md:-mt-1"
-								><path
-									fill-rule="evenodd"
-									d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-									clip-rule="evenodd"
-								/></svg
-							>
-						</span>
-					</button>
-					{#if dropdownOpen}
-						<div
-							transition:slide={{ delay: 50, duration: 150 }}
-							class="right-0 my-1 origin-top-right rounded-md shadow-lg"
+				<button
+					on:click|stopPropagation={() => (dropdownOpen = !dropdownOpen)}
+					class="py-0.5 mt-4 parent w-full"
+				>
+					<span class:!bg-gray-200={isCalcPage} class="menuItem child transition-all">
+						<span>Calculators</span>
+						<svg
+							fill="currentColor"
+							viewBox="0 0 20 20"
+							class:rotate-180={dropdownOpen}
+							class:rotate-0={!dropdownOpen}
+							class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-150 transform md:-mt-1"
+							><path
+								fill-rule="evenodd"
+								d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+								clip-rule="evenodd"
+							/></svg
 						>
-							<div class="px-2 py-2 bg-white rounded-md shadow">
-								{#each Object.entries(calculatorPages) as [path, title]}
-									<a class="py-0.5 block parent" on:click={() => (sidebarOpen = false)} href={path}
-										><div class="menuItem" class:!bg-gray-200={path === currentPath}>
-											{title}
-										</div>
-									</a>
-								{/each}
-							</div>
+					</span>
+				</button>
+				{#if dropdownOpen}
+					<div
+						transition:slide={{ delay: 50, duration: 150 }}
+						class="right-0 my-1 origin-top-right rounded-md shadow-lg"
+					>
+						<div class="px-2 py-2 bg-white rounded-md shadow">
+							{#each Object.entries(calculatorPages) as [path, title]}
+								<a class="py-0.5 block parent" on:click={() => (sidebarOpen = false)} href={path}
+									><div class="menuItem" class:!bg-gray-200={path === currentPath}>
+										{title}
+									</div>
+								</a>
+							{/each}
 						</div>
-					{/if}
-				</div>
+					</div>
+				{/if}
 				{#each Object.entries(mainPages) as [path, title]}
 					<a class="py-0.5 block parent" on:click={() => (sidebarOpen = false)} href={path}>
 						<div class:!bg-gray-200={path === currentPath} class="menuItem text-gray-900">
