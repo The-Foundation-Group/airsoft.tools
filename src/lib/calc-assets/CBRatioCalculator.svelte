bb3676af6548a54fc24acc136d52ed1f1909a267
MJA <Arsoth@users.noreply.github.com>
2024-07-19 15:54:23 -0400

    new AOE svg

--- src/lib/calc-assets/CBRatioCalculator.svelte
+++ src/lib/calc-assets/CBRatioCalculator.svelte
@@ -44,7 +44,7 @@
 	let bestRatioString = '';
 	let recommendedBarrelString = '';
 	let gunType = ['AEG', 'Bolt'];
-	let advancedMode = true;
+	let advancedMode = false;
 	const bbObject: SpecsObject = { maxValue: 3, maxLength: 5 };
 	const barrelLengthObject: SpecsObject = { maxValue: 1000, maxLength: 5 };
 	const volumeReductionObject: SpecsObject = { maxValue: 20, maxLength: 5 };
@@ -52,7 +52,7 @@
 	const cylIdObject: SpecsObject = { maxValue: 30, maxLength: 5 };
 	const portLengthObject: SpecsObject = { maxValue: 100, maxLength: 5 };
 	let infoOpen = false;
-	let moreAdvanced = true;
+	let moreAdvanced = false;
 
 	function calculateRatio(event: { preventDefault: () => void }) {
 		event.preventDefault();
@@ -143,8 +143,11 @@
 	<CalcHeader title="Cylinder Ratio" bind:open={infoOpen} />
 	{#if infoOpen}
 		<div class="calcInfoBox" transition:slide={{ delay: 10, duration: 150 }}>
+			<p class="mb-2">
+				<b>Advanced</b> allows for shortstroking and Airbreaks as well as custom AOE, cylinder sizes, and porting.
+			</p>
 			<p>
-				Max piston stroke and cyl head thickness are taken into account for volume calculations.
+				<b>Pickup to Cyl</b> is the distance between the furthest back point of the pickup tooth on the piston and the rear edge of the cylinder, which is the most consistent way to measure AOE.
 			</p>
 		</div>
 	{/if}
@@ -195,7 +198,7 @@
 					</div>
 					<span class="grow basis-0 p-0 justify-center">
 						<label for="aoeFixed" class="calcLabel">
-							<span class="label-text" style="width: 100%; text-align:center">AOE fixed</span>
+							<span class="label-text" style="width: 100%; text-align:center">AOE Fixed</span>
 						</label>
 						<input
 							class="hidden peer"
@@ -210,33 +213,28 @@
 							class="btn btn-outline btn-primary join-item no-animation grow basis-0 p-0 justify-center text-lg font-bold peer-checked:btn-primary hover:btn-primary !outline-none peer"
 							style="width: 100%"
 						>
-							<div class="">
+							<div>
 								<svg
 									class="justify-center"
-									fill="black"
 									aria-hidden="true"
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 512 512"
 									height="40px"
-									><path
-										d="M65.317,188.515l-29.328,44.379l19.202,19.145l43.375-28.765c7.306,4.562,15.271,8.549,23.734,10.997l10.566,52.604
-							h27.072l10.308-51.331c8.616-1.989,16.792-5.279,24.337-9.438l44.379,29.356l19.145-19.191l-28.765-43.366
-							c4.562-7.306,7.718-15.271,10.165-23.734l51.771-10.567v-27.071l-50.5-10.309c-1.988-8.616-4.867-16.792-9.017-24.327
-							l29.568-44.38l-19.088-19.144l-43.317,28.764c-7.306-4.562-15.243-7.812-23.706-10.27L164.671,0H137.6l-10.309,50.595
-							c-8.616,1.989-16.792,4.915-24.336,9.075l-44.38-29.539L39.431,49.228l28.764,43.318c-4.562,7.315-8.645,15.243-11.093,23.706
-							L4.404,126.799v27.071l51.427,10.309C57.82,172.794,61.157,180.97,65.317,188.515z M148.769,101.889
-							c22.539,0,40.812,18.273,40.812,40.812s-18.274,40.813-40.812,40.813c-22.539,0-40.813-18.274-40.813-40.813
-							S126.23,101.889,148.769,101.889z"
-									/>
-									<path
-										d="M263.834,202.361l9.228,51.188c-7.268,5.029-13.722,10.939-19.201,17.585l-52.106-10.996l-10.729,24.853l42.726,29.682
-							c-1.549,8.482-1.979,17.203-1.128,25.972l-44.667,29.09l9.983,25.158l51.169-9.218c5.029,7.268,10.93,13.731,17.575,19.201
-							l-11.007,52.106l24.854,10.729l29.682-42.725c8.482,1.549,17.184,1.95,25.962,1.109l29.08,44.647l25.159-9.983l-9.209-51.15
-							c7.268-5.029,13.731-10.92,19.211-17.566l52.116,11.007l10.729-24.853l-42.725-29.673c1.549-8.481,1.979-17.193,1.138-25.972
-							l44.666-29.089l-9.983-25.159l-51.169,9.218c-5.029-7.267-10.93-13.731-17.575-19.201l11.006-52.106l-24.853-10.729
-							l-29.682,42.726c-8.482-1.55-17.203-1.999-25.981-1.157l-29.099-44.686L263.834,202.361z M312.086,293.674
-							c20.952-8.319,44.677,1.932,52.996,22.883c8.319,20.952-1.932,44.677-22.884,52.996c-20.951,8.319-44.676-1.932-52.995-22.884
-							C280.894,325.708,291.135,301.983,312.086,293.674z"
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 80 80"
+									><defs
+										><style>
+											.cls-1 {
+												fill: #000;
+												stroke: #000;
+												stroke-miterlimit: 10;
+												stroke-width: 2px;
+											}
+										</style></defs
+									><title>Artboard 1transparent</title><polygon
+										class="cls-1"
+										points="82.23 -2.75 8.98 -2.75 8.98 39.81 10.09 50.77 16.83 50.77 21.42 39.81 34.47 39.81 36.17 44.5 41.67 44.5 43.53 39.81 50.45 39.81 52.16 49.11 57.66 49.11 59.53 39.81 66.47 39.81 68.17 49.11 73.66 49.11 75.52 39.81 82.23 39.81 82.23 -2.75"
+									/><path
+										class="cls-1"
+										d="M69.31,79.27h0l4.46-7.46-2-2.58L63.58,71.3h0a50.63,50.63,0,0,0-3.7-3.77h0L62.2,59l-2.64-1.91-7.62,4.56h0A48.53,48.53,0,0,0,46.87,59h0l-.55-8.6-3.08-1.07-5.9,6.53h0A48.66,48.66,0,0,0,32.18,55h0l-3.35-8.12H25.58L22.14,55h0A48.91,48.91,0,1,0,69.31,79.27Z"
 									/></svg
 								>
 							</div>
